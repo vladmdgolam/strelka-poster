0d0c74f9406a87b6f0c0dab0bd9a5c066f6c2062
Vlad <vladmdgolam@ya.ru>
2021-09-22 13:26:41 +0300

    refactor: clean cam

--- components/camera.js
+++ components/camera.js
@@ -2,7 +2,7 @@ import { PerspectiveCamera, OrbitControls } from "@react-three/drei"
 import { useThree } from "@react-three/fiber"
 import { button, useControls } from "leva"
 import { useMemo, useRef } from "react"
-import { MathUtils, Vector3 } from "three"
+import { MathUtils } from "three"
 
 const fov = 50
 
@@ -16,21 +16,12 @@ const Camera = () => {
 
   const dist = useMemo(() => {
     const vFOV = MathUtils.degToRad(fov)
-    // var height = 2 * Math.tan(vFOV / 2) * dist // visible height
     const dist = height / (2 * Math.tan(vFOV / 2))
     return dist
-
-    //   const wh = height
-    //   const a = wh / (2 * Math.cos(fov * (Math.PI / 180)))
-    //   console.log(a, wh)
-    //   const square = a ** 2 - wh ** 2 / 4
-    //   console.log(a, Math.sqrt(square), wh)
-    //   return Math.sqrt(square)
   }, [height])
 
   const resetCamera = () => {
     if (cam.current) {
-      // controls.current.reset()
       controls.current.target.set(0, 0, 0)
       cam.current.position.set(0, dist, 0)
       cam.current.rotation.set(-Math.PI / 2, 0, 0)
