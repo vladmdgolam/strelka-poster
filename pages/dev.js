0c66176bbed3873b1a05eb85a8a74a1b5d2d0d5b
Vlad <vladmdgolam@ya.ru>
2021-09-27 22:56:51 +0300

    feat: update dev.js

--- pages/dev.js
+++ pages/dev.js
@@ -1,18 +1,32 @@
 import Canvas from "@/components/canvas"
+import ControlsBtn from "@/components/controls/ControlsBtn"
 import Scene from "@/components/scene"
 import { randomStrelkaColor } from "@/helpers"
+import AppContext from "@/hooks/AppContext"
+import { useContextBridge } from "@react-three/drei"
 import { Leva } from "leva"
 import { useState } from "react"
-import { Perf } from "r3f-perf"
 
 const Home = () => {
   const [color] = useState(randomStrelkaColor())
+  const [random, setRandom] = useState(0)
+  const randomize = () => {
+    setRandom(random + 1)
+  }
+
+  const ContextBridge = useContextBridge(AppContext)
+
   return (
     <>
-      <Leva collapsed />
+      <Leva />
+
       <Canvas color={color}>
-        <Perf position="bottom-right" />
-        <Scene color={color} />
+        <ContextBridge>
+          <ControlsBtn onClick={randomize} position={8}>
+            🔀
+          </ControlsBtn>
+          <Scene random={random} color={color} />
+        </ContextBridge>
       </Canvas>
     </>
   )
