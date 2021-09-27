9c8c652e34d2040f90d25f7f4387de123e574def
Vlad <vladmdgolam@ya.ru>
2021-09-27 16:29:19 +0300

    fix: return

---
+++ components/controls/ControlsBtn.js
@@ -0,0 +1,38 @@
+import useIsMobile from "@/hooks/useIsMobile"
+import { useEffect } from "react"
+import HtmlWrapper from "../html/html"
+const rows = 2
+
+const ControlsBtn = ({ position = 1, name = "click", children, ...props }) => {
+  const isMobile = useIsMobile()
+
+  const cols = isMobile ? 5 : 12
+
+  useEffect(() => {
+    console.log(isMobile)
+  }, [isMobile])
+
+  const gridColumn = position % cols
+  const gridRow = rows - Math.floor(position / (cols + 1))
+  return (
+    <HtmlWrapper zIndexRange={[100, 0]}>
+      <div
+        className="controls"
+        style={{
+          gridTemplateColumns: `repeat(${cols}, 1fr)`,
+          gridTemplateRows: `repeat(${rows}, 1fr)`,
+        }}
+      >
+        <button
+          {...props}
+          className="btn btn_round"
+          style={{ gridColumn, gridRow }}
+        >
+          {children ? children : name}
+        </button>
+      </div>
+    </HtmlWrapper>
+  )
+}
+
+export default ControlsBtn
