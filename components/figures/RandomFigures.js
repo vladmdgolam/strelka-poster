2d239acbc0073f1d7575cbf139c553b6295f413d
Vlad <vladmdgolam@ya.ru>
2021-09-27 23:29:49 +0300

    feat: add button for more shapes

--- components/figures/RandomFigures.js
+++ components/figures/RandomFigures.js
@@ -1,6 +1,8 @@
 import { randomNumber } from "@/helpers"
 import { sizeScale as initSizeScale } from "@/helpers/constants"
+// import { useControls } from "leva"
 import { useEffect, useState } from "react"
+import ControlsBtn from "../controls/ControlsBtn"
 import RandomSpheres from "../figures/RandomSpheres"
 import RandomBoxes from "./RandomBoxes"
 import RandomCylinders from "./RandomCylinders"
@@ -11,11 +13,19 @@ const RandomFigures = ({ random }) => {
   const [number, setNumber] = useState(randomNumber(...range))
   const [sizeScale, setSizeScale] = useState(initSizeScale)
 
+  // useControls({
+  //   number: { value: number, onChange: (inv) => setNumber(inv) },
+  //   sizeScale: {
+  //     value: sizeScale,
+  //     onChange: (sizeScale) => setSizeScale(sizeScale),
+  //   },
+  // })
+
   // handle randomize
   useEffect(() => {
     if (random != 0) {
-      setNumber(randomNumber(...range))
       setSizeScale(1, 5)
+      setNumber(randomNumber(...range))
     }
   }, [random])
 
@@ -26,6 +36,15 @@ const RandomFigures = ({ random }) => {
 
   return (
     <>
+      {/* TODO: make it red when full */}
+      <ControlsBtn
+        onClick={() =>
+          setNumber(number < 100 ? number + 25 : randomNumber(...range))
+        }
+        position={7}
+      >
+        +⚪️⬜️
+      </ControlsBtn>
       <RandomSpheres {...childProps} />
       <RandomBoxes {...childProps} />
       <RandomCylinders {...childProps} />
